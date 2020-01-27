723e96d1858d0c81601cb5071c76a1c0376a589f
Nikolas Laus <bchik3@gmail.com>
2020-01-27 20:28:34 +0200

    Added search with date.

--- TTHKReplacementsBot.py
+++ TTHKReplacementsBot.py
@@ -6,13 +6,14 @@ from vk_api.keyboard import VkKeyboard, VkKeyboardColor, VkKeyboardButton
 import json
 output_rows = []
 writeyourgroup = {}
+writeyourdate = {}
 usergroup = {}
 # token : 4753258aa36e727b82691af62ec3425da7e41b82afc62cac1d0fbbf401cdaad837c069ec9ed4f5beb59c4
 # клавиатура
 keyboard = VkKeyboard(one_time=False, inline=False)
 
 keyboard.add_button('Изменения моей группы', color=VkKeyboardColor.PRIMARY)
-keyboard.add_button('Изменения всех групп', color=VkKeyboardColor.DEFAULT)
+keyboard.add_button('Изменения по датам', color=VkKeyboardColor.DEFAULT)
 keyboard.add_line()  # Переход на вторую строку
 keyboard.add_button('В какой я группе?', color=VkKeyboardColor.POSITIVE)
 keyboard.add_button('Изменить группу', color=VkKeyboardColor.NEGATIVE)
@@ -68,21 +69,32 @@ def parsepage(table):
                     data = cell.text
                     muudatus.append(data)
             # здесь есть полноценный список muudatus
-            print(muudatused)
             if muudatus != []:
                 muudatused.append(muudatus)
         else:
             continue
-    print(muudatused)
     return muudatused
 
 def getmuudatused(setgroup, usergroup, user):
     forshow = []
     muudatused = parsepage(table)
-    print(muudatused)
     for i in muudatused:
         if setgroup in i[2]:
-            if i[4].lower() == "jääb ära":
+            try:
+                print(i[4])
+            except IndexError:
+                forshow.append(f"{DayOfWeek[i[0]]} {i[1]} Группа: {i[2]} Урок: {i[3]}")
+                continue
+            try:
+                print(i[5])
+            except IndexError:
+                forshow.append(f"{DayOfWeek[i[0]]} {i[1]} Группа: {i[2]} Урок: {i[3]} Преподаватель: {i[4]}")
+                continue
+            if i[4] == " ":
+                forshow.append(f"{DayOfWeek[i[0]]} {i[1]} Группа: {i[2]} Урок: {i[3]}")
+            elif i[5] == " ":
+                forshow.append(f"{DayOfWeek[i[0]]} {i[1]} Группа: {i[2]} Урок: {i[3]} Преподаватель: {i[4]}")
+            elif i[4].lower() == "jääb ära":
                 forshow.append(f"{DayOfWeek[i[0]]} {i[1]} Группа: {i[2]} Урок: {i[3]} не состоится")
             elif i[4].lower() == "söögivahetund":
                 forshow.append(f"{DayOfWeek[i[0]]} {i[1]} Группа: {i[2]} Урок: {i[3]} обеденный перерыв")
@@ -97,26 +109,51 @@ def getmuudatused(setgroup, usergroup, user):
     elif len(forshow) == 0:
         write_msg(user, event.random_id,"Для вашей группы изменений в расписании нет. Подробнее: www.tthk.ee/tunniplaani-muudatused.")
 
-def getmuudatusedall(user):
-    forshow = []
+def getmuudatusedall(user, date):
     forshowall = []
     muudatused = parsepage(table)
-    print(muudatused)
     for i in muudatused:
-        if i[4].lower() == "jääb ära":
-            forshowall.append(f"{DayOfWeek[i[0]]} {i[1]} Группа: {i[2]} Урок: {i[3]} не состоится")
+        try:
+            i[4]
+        except IndexError:
+            if i[1] == date:
+                forshowall.append(f"{DayOfWeek[i[0]]} {i[1]} Группа: {i[2]} Урок: {i[3]}")
+            continue
+        try:
+            i[5]
+        except IndexError:
+            if i[1] == date:
+                forshowall.append(f"{DayOfWeek[i[0]]} {i[1]} Группа: {i[2]} Урок: {i[3]} Преподаватель: {i[4]}")
+            continue
+            # other
+        if i[4] == " ":
+            if i[1] == date:
+                forshowall.append(f"{DayOfWeek[i[0]]} {i[1]} Группа: {i[2]} Урок: {i[3]}")
+        elif i[5] == " ":
+            if i[1] == date:
+                forshowall.append(f"{DayOfWeek[i[0]]} {i[1]} Группа: {i[2]} Урок: {i[3]} Преподаватель: {i[4]}")
+        elif i[4].lower() == "jääb ära":
+            if i[1] == date:
+                forshowall.append(f"{DayOfWeek[i[0]]} {i[1]} Группа: {i[2]} Урок: {i[3]} не состоится")
         elif i[4].lower() == "söögivahetund":
-            forshowall.append(f"{DayOfWeek[i[0]]} {i[1]} Группа: {i[2]} Урок: {i[3]} обеденный перерыв")
+            if i[1] == date:
+                forshowall.append(f"{DayOfWeek[i[0]]} {i[1]} Группа: {i[2]} Урок: {i[3]} обеденный перерыв")
         elif i[5].lower() == "iseseisev töö kodus":
-            forshowall.append(f"{DayOfWeek[i[0]]} {i[1]} Группа: {i[2]} Урок: {i[3]} самостоятельная работа дома")
+            if i[1] == date:
+                forshowall.append(f"{DayOfWeek[i[0]]} {i[1]} Группа: {i[2]} Урок: {i[3]} самостоятельная работа дома")
         else:
-            forshowall.append(f"{DayOfWeek[i[0]]} {i[1]} Группа: {i[2]} Урок: {i[3]} Преподаватель: {i[4]} Кабинет: {i[5]}")
-    if len(forshow) > 0:
-        write_msg(event.user_id, event.random_id, f"В учебном заведении на данный момент следующие изменения в расписании:")
-        for w in forshow:
-            write_msg(event.user_id, event.random_id, w)
-    elif len(forshow) == 0:
-        write_msg(user, event.random_id,"В данный момент изменений в расписании нет. Подробнее: www.tthk.ee/tunniplaani-muudatused.")
+            if i[1] == date:
+                forshowall.append(f"{DayOfWeek[i[0]]} {i[1]} Группа: {i[2]} Урок: {i[3]} Преподаватель: {i[4]} Кабинет: {i[5]}")
+            else:
+                print("There is just nothing.")
+    if len(forshowall) > 0:
+        write_msg(user, event.random_id, f"В учебном заведении на {date} следующие изменения в расписании:")
+        kogutunniplaan = ""
+        for w in forshowall:
+            kogutunniplaan += f"{w}\n"
+        write_msg(user, event.random_id, kogutunniplaan)
+    elif len(forshowall) == 0:
+        write_msg(user, event.random_id,"В данный момент изменений в расписании нет на дату, которую вы ввели. Подробнее: www.tthk.ee/tunniplaani-muudatused.")
 
 
 longpoll = VkLongPoll(vk)
@@ -126,7 +163,6 @@ for event in longpoll.listen():
             if event.text.lower() == "начать":
                 usergroup = openfromfile(usergroup)
                 send_keyboard(event.peer_id, event.random_id, "Выберите вариант из клаиватуры ниже.")
-                print(usergroup.keys())
                 if str(event.user_id) not in usergroup.keys():
                     write_msg(event.user_id, event.random_id, "У вас не указан код группы.")
             elif event.text.lower() == "указать группу" or event.text.lower() == "изменить группу":
@@ -134,10 +170,8 @@ for event in longpoll.listen():
                 writeyourgroup[event.user_id] = 1
             elif event.text[-3:].lower() in ['v19', 'v18', 'v17', 'e19', 'e18', 'e17'] and writeyourgroup[event.user_id] == 1:
                 group = event.text
-                print(group)
                 usergroup[str(event.user_id)] = group
                 write_msg(event.user_id, event.random_id, f"Вы указали, что Ваша группа: {usergroup[str(event.user_id)]}.")
-                print(usergroup)
                 writeyourgroup[event.user_id] = 0
                 usergroup = updatefile(usergroup)
             elif event.text.lower() == "в какой я группе?":
@@ -148,8 +182,10 @@ for event in longpoll.listen():
             elif event.text.lower() == "изменения моей группы":
                 setgroup = usergroup[str(event.user_id)]
                 lastmuudatused = getmuudatused(setgroup, usergroup, event.user_id)
-            elif event.text.lower() == "изменения всех групп":
-                newmuudatused = getmuudatusedall(event.user_id)
+            elif event.text.lower() == "изменения по датам":
+                write_msg(event.user_id, event.random_id, f"Укажите дату которую желаете найти в формате ДД.ММ.ГГГГ:")
+                writeyourdate[str(event.user_id)] = 1
+            elif event.text[-5:].lower() in ['.2020', '.2021', '.2022', '.2023', '.2024', '.2025', '.2026'] and writeyourdate[str(event.user_id)] == 1:
+                newmuudatused = getmuudatusedall(event.user_id, event.text)
             else:
                 write_msg(event.user_id, event.random_id, f"Данной команды не существует.")
-                print(event.text)
