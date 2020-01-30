8f4ba90128316ac0c4fedefb24205d956f7c4732
Nikolas Laus <bchik3@gmail.com>
2020-01-31 01:09:48 +0200

    sender py

--- sender.py
+++ sender.py
@@ -1,16 +1,69 @@
-from bot import openfromfile, getmuudatused, sendeveryday
+
 import time
 print("Sender launched")
 
+def openfromfile(usergroup):
+    connection = pymysql.connect(
+        host='eu-cdbr-west-02.cleardb.net',
+        user=mysql_l,
+        password=mysql_p,
+        db='heroku_0ccfbccd1823b55',
+        cursorclass=DictCursor)
+    with connection.cursor() as cursor:
+        cursor.execute("""SELECT * FROM USERS""")
+        row = cursor.fetchall()
+#        print(row)
+        for i in row:
+#            print(i)
+            usergroup[i['vkid']] = i['thkruhm']
+    cursor.close()
+    connection.close()
+    return usergroup
+
+def makemuudatused(i, forshow):
+    if len(i) == 6:
+        forshow.append(f"🗓 {i[0]} Дата: {i[1]}\n🦆 Группа: {i[2]} ⏰ Урок: {i[3]} \n👨‍🏫 Преподаватель: {i[4]}\nКабинет: {i[5]}\n")
+    elif len(i) > 2 and i[3].lower() in "jääb ära":
+        forshow.append(f"🗓 {i[0]} Дата: {i[1]}\n🦆 {i[2]}\n❌ Не состоится\n")
+    elif len(i) > 4 and i[4].lower() in "jääb ära":
+        forshow.append(f"🗓 {i[0]} Дата: {i[1]}\n🦆 Группа: {i[2]} ⏰ Урок: {i[3]}\n❌ Не состоится\n")
+    elif len(i) > 4 and i[4].lower() in "söögivahetund":
+        forshow.append(f"🗓 {i[0]} Дата: {i[1]}\n🦆 Группа: {i[2]}\n ⏰ Урок: {i[3]}\n🆒 Обеденный перерыв\n")
+    elif len(i) > 5 and i[5].lower() in "iseseisev töö kodus":
+        forshow.append(f"🗓 {i[0]} Дата: {i[1]}\n🦆 Группа: {i[2]} ⏰ Урок: {i[3]}\n🏠 Самостоятельная работа дома\n")
+    elif len(i) > 5 and i[5].lower() in "iseseisev töö":
+        forshow.append(f"🗓 {i[0]} Дата: {i[1]}\n🦆 Группа: {i[2]} ⏰ Урок: {i[3]}\n📋 Самостоятельная работа\n")
+    elif len(i) > 5 and (i[5].lower() == "" or i[5].lower() == " "):
+        forshow.append(f"🗓 {i[0]} Дата: {i[1]}\n🦆 Группа: {i[2]} ⏰ Урок: {i[3]}\n👨‍🏫 Преподаватель: {i[4]}\n")
+    else:
+        forshow.append(f"🗓 В {i[0]} Дата: {i[1]}\n🦆 Группа: {i[2]} ⏰ Урок: {i[3]}\n")
+    return forshow
+
+def getmuudatused(setgroup, user):
+    forshow = []
+    muudatused = parsepage(table)
+    for i in muudatused:
+        if setgroup.lower() in i[2].lower():
+            makemuudatused(i, forshow)
+    if len(forshow) > 0:
+        kogutunniplaan = f"Для группы 🦆 {setgroup} на данный момент следующие изменения в расписании:\n"
+        for w in forshow:
+            kogutunniplaan += f"{w}\n"
+        write_msg(user, event.random_id, kogutunniplaan)
+    elif len(forshow) == 0:
+        write_msg(user, event.random_id, "Для группы, которую вы указали изменений в расписании нет.")
+
 def sendeveryday():
+    usergroup = {}
     usergroup = openfromfile(usergroup)
     print("Запускаю рассылку:")
     print(time.strftime("%H:%M:%S"))
     for i in usergroup.keys():
         getmuudatused(usergroup[i], i)
 
+
 while True:
-    if time.strftime("%H:%M:%S", time.localtime()) == '23:08:30':
+    if time.strftime("%H:%M:%S", time.localtime()) == '23:12:00':
         sendeveryday()
     time.sleep(1.1)
     continue
