14065d8b95e3db4133222a65771666f602ddee4c
Nikolas Laus <bchik3@gmail.com>
2020-01-30 11:18:42 +0200

    Fix #9

--- bot.py
+++ bot.py
@@ -359,4 +359,4 @@ for event in longpoll.listen():
                 write_msg(event.peer_id, event.random_id,"https://www.paypal.me/blinchk")
             else:
                 write_msg(event.user_id, event.random_id, f"Данной команды не существует.")
-            usergroup = uploadfile()
+            usergroup = updatefile()
