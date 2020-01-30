cade8a69ad2e4f9252adaf7a263685f9b5e5fe43
Nikolas Laus <bchik3@gmail.com>
2020-01-30 04:47:50 +0200

    Some error

--- bot.py
+++ bot.py
@@ -157,8 +157,8 @@ def updatefile():
 def openfromfile():
     global connection
     with connection.cursor() as cursor:
-    cursor.execute('''select * from users''')
-    cursor = cursor.fetchall()
+        cursor.execute('''select * from users''')
+        cursor = cursor.fetchall()
     for i in cursor:
         usergroup[i[0]] = i[1]
     connection.close()
