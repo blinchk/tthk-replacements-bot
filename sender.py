eb1f8db78be32aa034684dbf615e4326b47e9e18
Nikolas Laus <bchik3@gmail.com>
2020-01-31 01:06:16 +0200

    www

--- sender.py
+++ sender.py
@@ -10,7 +10,7 @@ def sendeveryday():
         getmuudatused(usergroup[i], i)
 
 while True:
-    if time.strftime("%H:%M:%S", time.localtime()) == '23:05:00':
+    if time.strftime("%H:%M:%S", time.localtime()) == '23:08:30':
         sendeveryday()
     time.sleep(1.1)
     continue
