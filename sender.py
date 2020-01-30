e2c2ad6406a00dbc92dd4e53d98359aecc9f1f85
Nikolas Laus <bchik3@gmail.com>
2020-01-31 00:54:22 +0200

    sender fix

--- sender.py
+++ sender.py
@@ -1,8 +1,5 @@
-from bot import openfromfile, getmuudatused, parsepage, makemuudatused, write_msg
-import vk_api as vkapi
-import schedule
+from bot import *
 import time
-import os
 
 mysql_l = os.environ['MYSQL_LOGIN']
 mysql_p = os.environ["MYSQL_PASS"]
@@ -17,7 +14,7 @@ def sendeveryday():
         getmuudatused(usergroup[i], i)
 
 while True:
-    if time.strftime("%H:%M:%S") == "22:50:00":
+    if time.strftime("%H:%M:%S", time.localtime()) == '00:57:00':
         sendeveryday()
     time.sleep(1.1)
     continue
