d72b20feb937fa1be2e4a34f512fde299a60e31b
Nikolas Laus <bchik3@gmail.com>
2020-02-01 16:13:08 +0200

    text.

--- sender.py
+++ sender.py
@@ -84,7 +84,7 @@ def getmuudatused(setgroup, user):
         if setgroup.lower() in i[2].lower():
             makemuudatused(i, forshow)
     if len(forshow) > 0:
-        kogutunniplaan = f"Для группы 🦆 {setgroup} на данный момент следующие изменения в расписании:\n"
+        kogutunniplaan = f"Доброе утро! Для группы 🦆 {setgroup} на данный момент следующие изменения в расписании:\n"
         for w in forshow:
             kogutunniplaan += f"{w}\n"
         write_msg(user, (random.getrandbits(31) * random.choice([-1, 1])), kogutunniplaan)
