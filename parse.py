53b9cf5c8485aca7c8a2dbdf3b8c7857843b79aa
Nikolas Laus <bchik3@gmail.com>
2020-04-01 18:53:18 +0300

    Optimize.

--- parse.py
+++ parse.py
@@ -5,7 +5,7 @@ import urllib.request
 def getdata():
     url = 'https://raw.githubusercontent.com/okestonia/koroonakaart/master/koroonakaart/src/data.json'
     data = urllib.request.urlopen(url).read()
-    self.data = json.loads(data)
+    data = json.loads(data)
     covid = [data['confirmedCasesNumber'], data['testsAdministeredNumber'], data['recoveredNumber'],
              data['deceasedNumber'], data['activeCasesNumber']]
     print(covid)
