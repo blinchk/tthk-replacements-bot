b4a26712d708fc93c044063626bd93423f10f2ce
Nikolas Laus <bchik3@gmail.com>
2020-12-22 16:23:44 +0200

    Update sender.py for Heroku Schelduer

--- sender.py
+++ sender.py
@@ -99,14 +99,12 @@ def sendeveryday(justtable):
     for i in usergroup.keys():
         getmuudatused(usergroup[i], i, justtable)
 
-while True:
-    if time.strftime("%H:%M:%S", time.localtime()) == '06:20:00' and time.strftime("%w", time.localtime()) in ['1', '2',
-                                                                                                               '3', '4',
-                                                                                                               '5']:
-        r = requests.get('http://www.tthk.ee/tunniplaani-muudatused/')
-        html_content = r.text
-        soup = BeautifulSoup(html_content, 'html.parser')
-        justtable = soup.findChildren('table')
-        sendeveryday(justtable)
-    time.sleep(1.1)
-    continue
+def start_sender():
+    r = requests.get('http://www.tthk.ee/tunniplaani-muudatused/')
+    html_content = r.text
+    soup = BeautifulSoup(html_content, 'html.parser')
+    table = soup.findChildren('table')
+    sendeveryday(table)
+
+if __name__ == '__main__' and time.strftime("%w", time.localtime()) in ['1', '2, '3', '4', '5']:
+    start_sender()
